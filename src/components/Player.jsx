ca74f6d7eb414e4a3d7c6c7bfc64a536035519fa
colt <drownworb@gmail.com>
2024-11-10 23:26:31 +0800

    add player component

---
+++ src/components/Player.jsx
@@ -0,0 +1,33 @@
+import { useState } from "react";
+
+function Player() {
+  const [enteredPlayerName, setEnteredPlayerName] = useState(null);
+  const [submitted, setSubmitted] = useState(false);
+
+  function handleChange(event) {
+    setSubmitted(false);
+    setEnteredPlayerName(event.target.value);
+  }
+
+  function handleClick() {
+    setSubmitted(true);
+  }
+
+  return (
+    <section id="player">
+      <h2>Welcome {submitted ? enteredPlayerName : "unknown entity"}</h2>
+      <p>
+        <input
+          type="text"
+          name=""
+          id=""
+          onChange={handleChange}
+          value={enteredPlayerName}
+        />
+        <button onClick={handleClick}>Set Name</button>
+      </p>
+    </section>
+  );
+}
+
+export default Player;
