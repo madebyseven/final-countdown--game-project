e87a6cd6729e3867ba143e5e34bf9dd1d9aa9407
colt <drownworb@gmail.com>
2024-11-12 21:14:50 +0800

    add challenges to the demo project

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
+
 import "./App.css";
+
 import Player from "./components/Player";
+import TimerChallenge from "./components/TimerChallenge";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -16,6 +19,12 @@ function App() {
         </p>
       </header>
       <Player />
+      <div id="challenges">
+        <TimerChallenge title="Easy" targetTime={1} />
+        <TimerChallenge title="Not Easy" targetTime={5} />
+        <TimerChallenge title="Getting tough" targetTime={10} />
+        <TimerChallenge title="Pros only" targetTime={15} />
+      </div>
     </>
   );
 }
